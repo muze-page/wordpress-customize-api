7cf3caf56aedb89e26b4bfb591ff5e6885505f80
Muze-mac <13581261474@163.com>
2023-02-03 19:40:34 +0800

    更新图标方法

--- options/custom-control.php
+++ options/custom-control.php
@@ -20,7 +20,52 @@ $wp_customize->add_section( 'separator_section',
    )
 );
 
-//布局选择
+//布局选择控件
+if ( ! class_exists( 'WP_Customize_Control' ) )
+    return NULL;
+
+/**
+ * Class to create a custom layout control
+ */
+class Layout_Picker_Custom_Control extends WP_Customize_Control
+{
+      /**
+       * Render the content on the theme customizer page
+       */
+      public function render_content()
+       {
+            ?>
+                <label>
+                  <span class="customize-layout-control"><?php echo esc_html( $this->label ); ?></span>
+                  <ul>
+                    <li><img src="http://magick.plugin/wp-content/uploads/2013/09/dsc20050604_133440_34211.jpg" alt="Full Width" /><input type="radio" name="<?php echo $this->id; ?>" id="<?php echo $this->id; ?>[full_width]" value="1" /></li>
+                    <li><img src="/2cl.png" alt="Left Sidebar" /><input type="radio" name="<?php echo $this->id; ?>" id="<?php echo $this->id; ?>[left_sidebar]" value="1" /></li>
+                    <li><img src="/2cr.png" alt="Right Sidebar" /><input type="radio" name="<?php echo $this->id; ?>" id="<?php echo $this->id; ?>[right_sidebar]" value="1" /></li>
+                  </ul>
+                </label>
+            <?php
+       }
+}
+
+
+$wp_customize->add_setting(
+  'api_list_radio',
+  array(
+      'sanitize_callback' => 'wp_filter_nohtml_kses',
+  )
+);
+
+$wp_customize->add_control( new Layout_Picker_Custom_Control( 
+	$wp_customize, 
+	'api_list_radio', 
+	array(
+		'label'	=> __( '选择一篇特色文章', 'themename' ),
+		'section' => 'separator_section',
+    'type' => 'radio'
+	) 
+));
+
+
 
 //最新帖子下拉菜单（列出最新十篇文章控件）
 if( class_exists( 'WP_Customize_Control' ) ):
--- wp-customize-api.php
+++ wp-customize-api.php
@@ -19,16 +19,22 @@ require_once dirname( __FILE__ ) . '/options/custom-control.php' ;
 //测试控件
 //require_once dirname( __FILE__ ) . '/test.php' ;
 
-//add_action( 'wp_head', 'test_tops' );
-//
-//function test_tops() {
-//
-//    $a =  trailingslashit( dirname( __FILE__ ) ) . 'inc/customizer.php';
-//    $b = '我被加载咯';
-//    //$add_content = '<h1>'.p( test ).'</h1>';
-//    $add_content = '<h5>'.$b.'</h5>';
-//    echo $add_content;
-//}
-//;
+add_action( 'wp_head', 'test_tops' );
+
+function test_tops() {
+
+    $a =  plugin_dir_url( __FILE__ ) . 'public/css/customizer-css.css';
+    $b = '我被加载咯';
+    //$add_content = '<h1>'.p( test ).'</h1>';
+    $add_content = '<h5>'.$a.'</h5>';
+    echo $add_content;
+}
+;
+
+//加载图标
+function THEMENAME_custom_customize_enqueue() {
+    wp_enqueue_style( 'customizer-css', plugin_dir_url( __FILE__ ) . 'public/css/customizer-css.css' );
+    }
+    add_action( 'customize_controls_enqueue_scripts', 'THEMENAME_custom_customize_enqueue' );
 
 ?>
